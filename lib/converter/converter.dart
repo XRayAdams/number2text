9082d74bb9d7aa076ba8e0b4ded9eed4e65d7b0a
Konstantin Adamov <rayadams@outlook.com>
2025-10-03 20:10:32 -0700

    Adding Italian to list of converters

--- lib/converter/converter.dart
+++ lib/converter/converter.dart
@@ -7,6 +7,7 @@ import 'package:number2text/converter/lng/spanish_converter.dart';
 
 import 'lng/base_converter.dart';
 import 'lng/english_converter.dart';
+import 'lng/italian_converter.dart';
 
 class NumberConverter {
   final List<BaseConverter> baseConverters = [
@@ -15,5 +16,6 @@ class NumberConverter {
     FrenchConverter(),
     GermanConverter(),
     RussianConverter(),
+    ItalianConverter(),
   ];
 }
