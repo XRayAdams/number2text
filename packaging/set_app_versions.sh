90a37cb73990654dcf938663ce858256b667ed3b
Konstantin Adamov <xrayadamo@gmail.com>
2025-10-14 12:22:57 -0700

    Updating packaging script

--- packaging/set_app_versions.sh
+++ packaging/set_app_versions.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+echo "___________________________________________________________"
+echo "Setting app version in all relevant files..."
+
+
 # --- Configuration ---
 PUBSPEC_FILE="pubspec.yaml"
-DEBIAN_YAML_FILE="debian/debian.yaml"
-DEBIAN_DESKTOP_FILE="debian/gui/app.rayadams.number2text.desktop"
+DEBIAN_CONTROL_FILE="packaging/control"
+DEBIAN_DESKTOP_FILE="packaging/gui/app.rayadams.number2text.desktop"
 SNAP_YAML_FILE="snap/snapcraft.yaml"
 SNAP_DESKTOP_FILE="snap/gui/number2text.desktop"
-RPM_FILE="number2text.spec"
+RPM_FILE="packaging/number2text.spec"
+MACHINE_ARCH=$(uname -m)
+DEBIAN_CONTROL_FILE_ARCH="amd64"
+
+if [ "$MACHINE_ARCH" == "aarch64" ]; then
+    MACHINE_ARCH="arm64"
+    DEBIAN_CONTROL_FILE_ARCH="arm64"
+    echo "Architecture was aarch64, updated to: $MACHINE_ARCH"
+elif [ "$MACHINE_ARCH" == "x86_64" ]; then
+    MACHINE_ARCH="x64"
+    DEBIAN_CONTROL_FILE_ARCH="amd64"
+fi
 # ---------------------
 
 
@@ -15,8 +30,8 @@ if [ ! -f "$PUBSPEC_FILE" ]; then
     echo "Error: File not found: $PUBSPEC_FILE"
     exit 1
 fi
-if [ ! -f "$DEBIAN_YAML_FILE" ]; then
-    echo "Error: File not found: $DEBIAN_YAML_FILE"
+if [ ! -f "$DEBIAN_CONTROL_FILE" ]; then
+    echo "Error: File not found: $DEBIAN_CONTROL_FILE"
     exit 1
 fi
 if [ ! -f "$DEBIAN_DESKTOP_FILE" ]; then
@@ -46,15 +61,15 @@ fi
 
 echo "Version '$APP_VERSION' found in $PUBSPEC_FILE"
 
-# App details from pubspec.yaml
-APP_NAME=$(grep 'name:' pubspec.yaml | awk '{print $2}')
+# App details from pubspec.yaml for RPM spec
 APP_VERSION_SHORT=$(grep 'version:' pubspec.yaml | awk '{print $2}' | cut -d'+' -f1)
 APP_BUILD=$(grep 'version:' pubspec.yaml | awk '{print $2}' | cut -d'+' -f2)
 
-
 # Use sed to find and replace the Version line in debian.yaml and desktop file
 # This command looks for the line starting with '  Version:' and replaces the entire line.
-sed -i "s/^\(\s*Version:\s*\).*\$/\1$APP_VERSION/" "$DEBIAN_YAML_FILE"
+sed -i "s/^\(\s*Version:\s*\).*\$/\1$APP_VERSION/" "$DEBIAN_CONTROL_FILE"
+sed -i "s/^\(\s*Architecture:\s*\).*\$/\1$DEBIAN_CONTROL_FILE_ARCH/" "$DEBIAN_CONTROL_FILE"
+
 sed -i "s/^\(\s*Version=\s*\).*\$/\1$APP_VERSION/" "$DEBIAN_DESKTOP_FILE"
 
 sed -i "s/^\(\s*version:\s*\).*\$/\1$APP_VERSION/" "$SNAP_YAML_FILE"
